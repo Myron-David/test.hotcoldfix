ebd26ce1c007982ad94ecc8752b77b42db6ce0a8
Trey Ferguson <admin@hotcoldfix.com>
2025-06-10 22:00:18 -0500

    …

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>HotColdFix Test Build</h1>
+      <p>If you see this, the build is working!</p>
+    </div>
+  );
+}
